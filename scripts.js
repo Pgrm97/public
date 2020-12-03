b3a0649bc7e2678fac1fba3ff1ad3ffcb196bb67
Pgrm97 <Pgrm97@gmail.com>
2020-12-03 17:45:09 -0400

    Search API works

--- scripts.js
+++ scripts.js
@@ -3,39 +3,59 @@ const app = document.getElementById('root')
 const container = document.createElement('div')
 container.setAttribute('class', 'container')
 
-app.appendChild(container)
+app.appendChild(container);
 
-var request = new XMLHttpRequest()
+callAPI();
 
-request.open('GET', 'http://my-json-server.typicode.com/Pgrm97/public/events', true);
+function callAPI(){    
+    var request = new XMLHttpRequest()
 
-request.onload = function () {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach((event) => {
-      const card = document.createElement('div')
-      card.setAttribute('class', 'card')
+    var URL = "https://my-json-server.typicode.com/Pgrm97/public/events";
 
-      const h1 = document.createElement('h1')
-      h1.textContent = event.name
+    var type = document.getElementById("type").value;
 
-      const h2 = document.createElement('h2');
-      const p = document.createElement('p')
-      //event.description = event.description.substring(0, 300)
-      h2.textContent = `${event.city}`
-      p.textContent = `${event.category}`
+    var text = document.getElementById("gsearch").value;
 
-      container.appendChild(card)
-      card.appendChild(h1)
-      card.appendChild(h2)
-      card.appendChild(p)
-    })
-  } else {
-    const errorMessage = document.createElement('marquee')
-    errorMessage.textContent = `Gah, it's not working!`
-    app.appendChild(errorMessage)
-  }
+    var final_URL = URL + "?" + type + "=" + text;
+
+    console.log(text);
+
+    request.open('GET', final_URL , true);
+
+    request.onload = function () {
+    // Begin accessing JSON data here
+    var data = JSON.parse(this.response)
+    if (request.status >= 200 && request.status < 400) {
+        data.forEach((event) => {
+        const card = document.createElement('div')
+        card.setAttribute('class', 'card')
+
+        const h1 = document.createElement('h1')
+        h1.textContent = event.name
+
+        const h2 = document.createElement('h2');
+        const p = document.createElement('p')
+        //event.description = event.description.substring(0, 300)
+        h2.textContent = `${event.city}`
+        p.textContent = `${event.category}`
+
+        container.appendChild(card)
+        card.appendChild(h1)
+        card.appendChild(h2)
+        card.appendChild(p)
+        })
+    } else {
+        const errorMessage = document.createElement('marquee')
+        errorMessage.textContent = `Gah, it's not working!`
+        app.appendChild(errorMessage)
+    }
+    }
+
+    request.send();
 }
 
-request.send()
+function removeElement(elementId) {
+    // Removes an element from the document
+    var element = document.getElementById(elementId);
+    element.parentNode.removeChild(element);
+}
